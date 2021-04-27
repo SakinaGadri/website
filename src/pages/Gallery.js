61f589cde8a2f9f4e56b5b53948bb307d38c9ee3
sakina.gadriwala <sakina.gadriwala@mail.utoronto.ca>
2021-04-26 22:06:19 -0400

    displayed images

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -6,6 +6,22 @@ import styled from 'styled-components';
 /* images */
 import leaves from '../gallery/autumn-leaves.jpg';
 import autumnTrees from '../gallery/autumn-trees.jpg';
+import beach from '../gallery/beach.jpg';
+import book from '../gallery/Book.jpg';
+import esclator from '../gallery/esclator.jpg';
+import finger from '../gallery/finger.jpg';
+import grass from '../gallery/grass.jpg';
+import shoes from '../gallery/hello.jpg';
+import husnEyusuf from '../gallery/Husn-e-Yusuf.jpg';
+import keyboard from '../gallery/keyboard.jpg';
+import lakeAndSun from '../gallery/LakeAndSun.jpg';
+import mountains from '../gallery/mountains.jpg';
+import omw from '../gallery/omw.jpg';
+import oyster from '../gallery/oyster.jpg';
+import park from '../gallery/park.jpg';
+import pcarnival from '../gallery/pcarnival.jpg';
+import snail from '../gallery/snail.jpg';
+import symmtericalTrees from '../gallery/symmetrical-trees.jpg';
 
 // https://www.w3schools.com/howto/howto_css_image_grid_responsive.asp
 const Row = styled.div`
@@ -36,6 +52,11 @@ const Image = styled.img`
     width: 100%;
 `;
 
+const Icon = styled.img`
+    width: 40px;
+    margin-right: 10px;
+`;
+
 export default class Gallery extends React.Component {
     render() {
         return (
@@ -44,19 +65,40 @@ export default class Gallery extends React.Component {
                 <StyledDiv>
                     <Col>
                         <h3>
-                            <img alt="clap" src="https://www.flaticon.com/svg/static/icons/svg/941/941590.svg" style={{ width: "40px", marginRight: "10px" }} />
+                            <Icon src="https://www.flaticon.com/svg/static/icons/svg/941/941590.svg" />
                             Gallery
                         </h3>
                         <Row>
                             <Column>
                                 <Image src={leaves} />
                                 <Image src={autumnTrees} />
+                                <Image src={beach} />
+                                <Image src={pcarnival} />
+                                <Image src={park} />
+                            </Column>
+                            <Column>
+                                <Image src={shoes} />
+                                <Image src={esclator} />
+                                <Image src={finger} />
+                                <Image src={snail} />
+                                <Image src={symmtericalTrees} />
+                            </Column>
+                            <Column>
+                                <Image src={book} />
+                                <Image src={grass} />
+                                <Image src={omw} />
+                                <Image src={husnEyusuf} />
+                            </Column>
+                            <Column>
+                                <Image src={keyboard} />
+                                <Image src={lakeAndSun} />
+                                <Image src={mountains} />
+                                <Image src={oyster} />
                             </Column>
                         </Row>
                     </Col>
                 </StyledDiv>
                 <Footer />
-
             </div>
         );
     }
